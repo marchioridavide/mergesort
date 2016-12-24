ebab647cc2203699c1335638211f591e5bf1adc0
davide <davide.melkior@gmail.com>
2016-12-24 22:39:27 +0100

    mergesort

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeSort
+{
+    class MergeSort
+    {
+        static public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, eol, num, pos;
+
+            eol = (mid - 1);
+            pos = left;
+            num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+
+            for (i = 0; i < num; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void SortMerge(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                SortMerge(numbers, left, mid);
+                SortMerge(numbers, (mid + 1), right);
+
+                MainMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            
+            int[] numbers = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 50);
+            }
+            Console.WriteLine("MergeSort By Recursive Method");
+            SortMerge(numbers, 0, 10 - 1);
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine(numbers[i]);
+            Console.ReadLine();
+        }
+    }
+}
